1290b43f8ca0d5664cae1ad059676e97a3349295
Ryan Ye <ryanye@dhcp-172-23-116-91.sha.corp.google.com>
2012-05-08 15:45:53 +0800

    'git' package is not universally available. Only try ensuring the package if git binary couldn't be found

--- fabtask/vcs.py
+++ fabtask/vcs.py
@@ -1,10 +1,12 @@
 from fabric.contrib.files import exists
 from fabric.api import run, cd
 from fabtask.packages import ensure_package
+from fabtask.utils import program_exists
 
 def ensure_git_repo(path, url, pushurl=None, submodules=False):
 	if not exists(path):
-		ensure_package('git')
+		if not program_exists('git'):
+			ensure_package('git')
 		run('git clone %s %s' % (url, path))
 		if pushurl:
 			with(cd(path)):
